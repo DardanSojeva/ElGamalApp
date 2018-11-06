5519e9f88b839e6fb75e73a636f9d79aa95973bb
Dardan Sojeva <dardansojeva@yahoo.com>
2018-11-07 00:11:23 +0100

    sintaksë - test

--- ElGamalApp/ElGamalManaged.cs
+++ ElGamalApp/ElGamalManaged.cs
@@ -14,15 +14,18 @@ namespace ElGamalApp
         {
             // create the key struct
             o_key_struct = new ElGamalKeyStruct();
+            
             // set all of the big integers to zero
             o_key_struct.P = new BigInteger(0);
             o_key_struct.G = new BigInteger(0);
             o_key_struct.Y = new BigInteger(0);
             o_key_struct.X = new BigInteger(0);
             KeySizeValue = 1024;
+            
             // set the range of legal keys
             LegalKeySizesValue = new KeySizes[] { new KeySizes(384, 1088, 8) };
         }
+        
         public override string SignatureAlgorithm
         {
             get
@@ -45,8 +48,7 @@ namespace ElGamalApp
             Random x_random_generator = new Random();
 
             // create the large prime number, P
-            o_key_struct.P = BigInteger.genPseudoPrime(p_key_strength,
-                16, x_random_generator);
+            o_key_struct.P = BigInteger.genPseudoPrime(p_key_strength, 16, x_random_generator);
 
             // create the two random numbers, which are smaller than P
             o_key_struct.X = new BigInteger();
@@ -56,11 +58,13 @@ namespace ElGamalApp
 
             // compute Y
             o_key_struct.Y = o_key_struct.G.modPow(o_key_struct.X, o_key_struct.P);
+            
         }
         private bool NeedToGenerateKey()
         {
             return o_key_struct.P == 0 && o_key_struct.G == 0 && o_key_struct.Y == 0;
         }
+
 
         public ElGamalKeyStruct KeyStruct
         {
@@ -77,38 +81,44 @@ namespace ElGamalApp
                 o_key_struct = value;
             }
         }
+        
         public override void ImportParameters(ElGamalParameters p_parameters)
         {
             // obtain the  big integer values from the byte parameter values
             o_key_struct.P = new BigInteger(p_parameters.P);
             o_key_struct.G = new BigInteger(p_parameters.G);
             o_key_struct.Y = new BigInteger(p_parameters.Y);
+            
             if (p_parameters.X != null && p_parameters.X.Length > 0)
             {
                 o_key_struct.X = new BigInteger(p_parameters.X);
             }
+            
             // set the length of the key based on the import
             KeySizeValue = o_key_struct.P.bitCount();
+            
         }
 
-        public override ElGamalParameters ExportParameters(bool
-        p_include_private_params)
+        public override ElGamalParameters ExportParameters(bool p_include_private_params)
         {
 
             if (NeedToGenerateKey())
             {
+            
                 // we need to create a new key before we can export 
                 CreateKeyPair(KeySizeValue);
+                
             }
 
             // create the parameter set
             ElGamalParameters x_params = new ElGamalParameters();
+            
             // set the public values of the parameters
             x_params.P = o_key_struct.P.getBytes();
             x_params.G = o_key_struct.G.getBytes();
             x_params.Y = o_key_struct.Y.getBytes();
 
-            // if required, include the private value, X
+            // if required, include the private value, X   
             if (p_include_private_params)
             {
                 x_params.X = o_key_struct.X.getBytes();
@@ -130,7 +140,8 @@ namespace ElGamalApp
                 CreateKeyPair(KeySizeValue);
             }
             // encrypt the data
-            ElGamalEncryptor x_enc = new ElGamalEncryptor(o_key_struct);
+            ElGamalEncryptor x_enc = new ElGamalEncryptor(o_key_struct) 
+            
             return x_enc.ProcessData(p_data);
         }
 
@@ -143,12 +154,13 @@ namespace ElGamalApp
             }
             // encrypt the data
             ElGamalDecryptor x_enc = new ElGamalDecryptor(o_key_struct);
+            
             return x_enc.ProcessData(p_data);
         }
 
         protected override void Dispose(bool p_bool)
         {
-            // do nothing - no unmanaged resources to release
+          
         }
 
         public override byte[] Sign(byte[] p_hashcode)
@@ -168,8 +180,7 @@ namespace ElGamalApp
                 // we need to create a new key before we can export 
                 CreateKeyPair(KeySizeValue);
             }
-            return ElGamalSignature.VerifySignature(p_hashcode,
-              p_signature, o_key_struct);
+            return ElGamalSignature.VerifySignature(p_hashcode, p_signature, o_key_struct);
         }
     }
 }
